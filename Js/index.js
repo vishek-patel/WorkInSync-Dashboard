3d5b1b544f95bd3fdf8076282f45d9b089fb9622
Diganta Chaudhuri <digantachaudhuri03@gmail.com>
2023-03-09 12:33:33 +0530

    API structures

---
+++ Js/index.js
@@ -0,0 +1,25 @@
+// API -- 1 (/userstatus)
+const users = [
+    {
+        active_user: {
+            incriment: true,
+            percent: 12,
+            totalUser: 20.8,
+        },
+    },
+    {
+        active_session: {
+            incriment: false,
+            percent: 2,
+            totalSession: 25.1,
+        },
+    },
+    {
+        sessionDuration: {
+            incriment: true,
+            duration: 232,
+            percent: 9,
+
+        }
+    }
+]
