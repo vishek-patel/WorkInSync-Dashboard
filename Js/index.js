8f8c99b68781613acaa5c8169b568c621f81a329
Diganta Chaudhuri <digantachaudhuri03@gmail.com>
2023-03-09 13:17:10 +0000

    Added js of users

--- Js/index.js
+++ Js/index.js
@@ -1,8 +1,20 @@
+// Definations
 const div = document.querySelector(".total-users")
 const btn = document.querySelector(".real-time-report")
 const fill = document.querySelector(".fill")
 const countryDiv = document.querySelector(".countries")
 
+const user_count = document.querySelector('.user-count');
+const user_dataPercent = document.querySelector('.data-percent');
+const user_sessions = document.querySelector('.main-sessions');
+const user_sessions_percent = document.querySelector('.session-percent');
+const user_session_duration = document.querySelector('.session-duration');
+const user_session_duration_percent = document.querySelector('.session-duration-percent');
+
+// Images
+const duration_arrow = document.querySelector('.duration-arrow');
+const session_arrow = document.querySelector('.sessions-arrow');
+const user_arrow = document.querySelector('.user-arrow');
 
 const obj ={};
 const json = "";
@@ -67,3 +79,42 @@ filterButton.addEventListener('click', function() {
 
     }
 );
+
+
+
+// Fetching data from API
+const fetchData = async (url) => {
+    try {
+        const res = await fetch(url)
+        const data = await res.json();
+        return data;
+    } catch (err) {
+        throw err;
+    }
+}
+const secToMin = (sec) => {
+    const min = Math.floor(sec / 60);
+    const seconds = sec % 60;
+    return `${min}m ${seconds}s`;
+}
+const updateUserData = async () => {
+    try {
+        const data = await fetchData("https://d858c184-0058-4be6-8826-d26b9bd5e0b0.mock.pstmn.io/user-status");
+        user_count.innerHTML = data?.users[0]?.active_user?.totalUser + "k";
+        user_dataPercent.innerHTML = data?.users[0]?.active_user?.percent + "%";
+        user_session_duration.innerHTML = secToMin(data?.users[2]?.sessionDuration?.duration);
+        user_sessions.innerHTML = data?.users[1]?.active_session?.totalSession + "k";
+        user_sessions_percent.innerHTML = data?.users[1]?.active_session?.percent + "%";
+        // Incriment or Decriment
+        if (data?.users[0]?.active_user?.incriment) {
+            duration_arrow.src = "/assets/upArrow.svg";
+        } else {
+            duration_arrow.src = "/assets/downArrow.svg";
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+updateUserData()
